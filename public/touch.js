18b8dd89d9dc40275c8b53d8f3594a9a8c7c4fce
Glitch (invaders-from-space) <none>
2017-11-26 22:17:50 +0000

    Fixed some glitches in alien formation flights

--- public/touch.js
+++ public/touch.js
@@ -1,6 +1,27 @@
 document.body.addEventListener('touchstart', showControls);
 
 function showControls() {
+  
+  // Handling for itch.io iframe - not working
+  if(GameAudio.context.state === 'suspended') {
+    GameAudio.context.resume();
+    
+  // Other workaround attempts 
+  // if(GameAudio.context.state === 'suspended') {
+  //   try {
+  //     initAudio();
+  //   }
+  //   catch(e) { console.log(e); }      
+  // }
+  // if(GameAudio.context.state === 'suspended') {
+  //   GameAudio.context.resume().then(function() {
+  //     GameAudio.pillCollectSound();
+  //   },
+  //   function(reason) {
+  //     console.log(reason); 
+  //   });       
+  // 
+  }
   document.body.removeEventListener('touchstart', showControls);
   var touchControls = new TouchControls();
   app.stage.addChild(touchControls);
